6fc1bed3f6c89650959eb17fb2b39b137cbff936
Fabio Ricali <ricalifabio@gmail.com>
2020-06-13 18:09:10 +0200

    use defineWebComponent

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.2.0] - 2020-06-13
+## [1.2.1] - 2020-06-13
 - Compatibility with doz 3.3.1
 
 ## [1.1.5] - 2020-06-08
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cmp from './src'
 function register() {
     if (typeof window !== 'undefined') {
         define('doz-artwork-player', cmp);
-        defineWebComponent('doz-artwork-player', cmp, [
+        defineWebComponent('dwc-doz-artwork-player', cmp, [
             'image-url',
             'audio-url',
             'height',
